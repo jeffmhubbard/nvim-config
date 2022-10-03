f232811e8947f1ab8b481bcd69ddd77d7eb5eab6
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2022-10-02 19:40:15 -0500

    Remove DAP

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -88,11 +88,6 @@ return packer.startup(function(use)
   -- Git
   use { "lewis6991/gitsigns.nvim" }
 
-  -- DAP
-  use { "mfussenegger/nvim-dap" }
-  use { "rcarriga/nvim-dap-ui" }
-  use { "ravenxrz/DAPInstall.nvim" }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
