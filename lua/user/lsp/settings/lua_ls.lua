da7e9e29b17ad78f0febf51f5b714ecea038e6cb
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2023-04-08 06:20:29 -0500

    Add awesomewm globals to lua_ls

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -2,7 +2,7 @@ return {
   settings = {
     Lua = {
       diagnostics = {
-        globals = { "vim" },
+        globals = { "vim", "awesome", "screen", "tag", "client", "mouse" },
       },
       workspace = {
         library = {
