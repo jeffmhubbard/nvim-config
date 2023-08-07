36efa271fdf0edbe5cd3d4d93789ed23d562b3fc
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2023-08-07 03:30:51 -0500

    Fix missing nerdfonts

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -7,7 +7,7 @@ bufferline.setup {
   options = {
     close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
     right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    offsets = { { filetype = "NvimTree", highlight = "Directory", text = "", padding = 0 } },
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
     numbers = "ordinal",
   },
--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -12,10 +12,10 @@ M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 M.setup = function()
   local signs = {
 
+    { name = "DiagnosticSignHint",  text = "" },
+    { name = "DiagnosticSignInfo",  text = "" },
+    { name = "DiagnosticSignWarn",  text = "" },
     { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
   }
 
   for _, sign in ipairs(signs) do
